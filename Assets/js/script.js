269fc6e15a79c1995ade58b37ed613551874605c
Lisette33 <113862182+Lisette33@users.noreply.github.com>
2022-11-05 15:17:26 -0400

    Added update to HTML &  CSS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,13 @@
 var userContainer = document.getElementById('users');
 var fetchButton = document.getElementById('fetch-button');
 var APIKey = "bf7b44b0999819873a0d8c5e9db1d207";
-var city; 
+// var city = Miami; 
 var lat;
 var lon;
 
 // Weather for city
-function getApi() {
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=city&appid=bf7b44b0999819873a0d8c5e9db1d207';
+function getCurrentWeatherApi(city) {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=bf7b44b0999819873a0d8c5e9db1d207&units=imperial';
 
 // Browser Fetch Method
   fetch(requestUrl)
@@ -17,24 +17,21 @@ function getApi() {
     .then(function (data) {
       // Use the console to examine the response
       console.log(data);
-      // Loop through the data and generate your HTML
-         // My code and data is the response 
-      for (var i=0; i< data.length;i++) {
-        var userNaameContainer=document.createElement("h3");
-        userNaameContainer.innerText=data[i].login;
-        userContainer.appendChild(userNaameContainer);
-        var issueUrl=document.createElement("a");
-        issueUrl.innerText=data[i].html_url;
-        userContainer.appendChild(issueUrl);
-      }
+      // My code and data is the response 
+      var userNaameContainer=document.createElement("h3");
+      userNaameContainer.innerText=data.main.temp;
+      userContainer.appendChild(userNaameContainer);
+      var issueUrl=document.createElement("a"); 
+      issueUrl.innerText=data.wind.speed;
+      userContainer.appendChild(issueUrl);
+
     });
 }
-fetchButton.addEventListener('click', getApi);
 
 
 // Forecast for city
-function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=bf7b44b0999819873a0d8c5e9db1d207';
+function getForecastApi(lat, lon) {                                     
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=bf7b44b0999819873a0d8c5e9db1d207&units=imperial';
   
 //  Browser Fetch Method
     fetch(requestUrl)
@@ -46,7 +43,7 @@ function getApi() {
         console.log(data);
        // Loop through the data and generate your HTML
           //   My code and data is the response 
-        for (var i=0; i< data.length;i++) {
+        for (var i=0; i< data.list.length;i++) {
           var userNaameContainer=document.createElement("h3");
           userNaameContainer.innerText=data[i].login;
           userContainer.appendChild(userNaameContainer);
@@ -56,6 +53,25 @@ function getApi() {
         }
       });
   }
+
+// API for city name
+function getApi() {
+    var city = document.querySelector('#dates-input').value;
+    var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid=bf7b44b0999819873a0d8c5e9db1d207&units=imperial';
+
+    //  Browser Fetch Method
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+       // Use the console to examine the response
+        console.log(data);
+        console.log(data[0].lat,data[0].lon);
+    //  Loop through the data and generate your HTML
+            // My code and data is the response 
+    getForecastApi(data[0].lat,data[0].lon);
+    getCurrentWeatherApi(data[0].name);
+      });
+  }
   fetchButton.addEventListener('click', getApi);
-
-
